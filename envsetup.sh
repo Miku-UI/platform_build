6d209d56f7891d36b049dbe0c7c2f48fcdb8ea59
LuK1337 <priv.luk@gmail.com>
2024-03-11 11:28:46 +0100

    envsetup: Pass valid TARGET_RELEASE to check_product
    Change-Id: I709248c0d362cffdac57d9893b7dfb4d2329204c

--- envsetup.sh
+++ envsetup.sh
@@ -157,7 +157,7 @@ function check_product()
     export MIKU_BUILD
 
         TARGET_PRODUCT=$1 \
-        TARGET_RELEASE= \
+        TARGET_RELEASE=$2 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
         TARGET_BUILD_APPS= \
@@ -466,14 +466,14 @@ function _lunch_meat()
     local release=$2
     local variant=$3
 
-    if ! check_product $product
+    if ! check_product $product $release
     then
         # if we can't find a product, try to grab it off the Diva Room GitHub
         T=$(gettop)
         cd $T > /dev/null
         vendor/miku/build/tools/roomservice.py $product
         cd - > /dev/null
-        check_product $product
+        check_product $product $release
     else
         T=$(gettop)
         cd $T > /dev/null
