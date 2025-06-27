9a7036d3949ee0b504580efd994d47d8c584fe6c
AmeChanRain <downloadbot007@gmail.com>
2025-06-27 18:19:05 +0800

    [REVERT ME] base_system: Don't build com.android.ranging for now
    * It does not exist in AOSP.
    
    Change-Id: Ia26c554376d4d8238de0b0e59abafe3ccc3816e5

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -376,10 +376,10 @@ ifneq ($(RELEASE_MOVE_VCN_TO_MAINLINE),true)
         framework-connectivity-b
 endif
 
-ifneq (,$(RELEASE_RANGING_STACK))
-    PRODUCT_PACKAGES += \
-        com.android.ranging
-endif
+#ifneq (,$(RELEASE_RANGING_STACK))
+#    PRODUCT_PACKAGES += \
+#        com.android.ranging
+#endif
 
 ifeq ($(RELEASE_MEMORY_MANAGEMENT_DAEMON),true)
   PRODUCT_PACKAGES += \
