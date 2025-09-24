3495cd32946cd02148496de42be765014bdefa5a
Yumi Yukimura <me.cafebabe@gmail.com>
2025-09-24 23:04:55 +0800

    build: Disable `aosp_system_image` soong target for miku GSI
    Change-Id: I2cdb1f3cf1a00ba8fad7b8fc0745ed174a32a068

--- target/product/gsi_release.mk
+++ target/product/gsi_release.mk
@@ -27,6 +27,9 @@
 #
 
 BUILDING_GSI := true
+ifneq ($(filter miku_gsi_%,$(TARGET_PRODUCT)),)
+BUILDING_MIKU_GSI := true
+endif
 
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/etc/init/config \
